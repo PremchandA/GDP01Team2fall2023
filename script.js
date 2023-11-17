e4ebe35c8dba15dda5876f9d87d9ecfb624f58c6
PremchandA <123143572+PremchandA@users.noreply.github.com>
2023-11-16 21:41:05 -0600

    modified script.js file

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ function login() {
     // For demonstration purposes, let's consider a simple username/password check
     if (username === "user" && password === "password") {
         alert("Login successful!");
-    } else {
-        alert("Invalid username or password. Please try again.");
+    } 
+    else
+     {function authenticate(username, password) {
+        // Perform authentication (this is where you would typically make a server request)
+        // For demonstration purposes, let's consider a simple username/password check
+        if (username === "user" && password === "password") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function login() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+    
+        if (authenticate(username, password)) {
+            alert("Login successful!");
+        } else {
+            alert("Invalid username or password. Please try again. or contact Admin");
+        }
+    }
+    
+        alert("Invalid username or password. Please try again. or contact Admin");
     }
 }
